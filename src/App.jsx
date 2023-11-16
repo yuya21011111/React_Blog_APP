6fe7cddf0a731364904beb2644579e90c0a61d46
YuyaYamazi <kaimomo12345@gmail.com>
2023-11-16 14:23:04 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import Home from './components/Home'
+import Createpost from './components/Createpost'
+import Login from './components/Login'
+import Logout from './components/Logout'
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/createpost' element={<Createpost />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/logout' element={<Logout />}></Route>
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App
