e359c14db128a501e4bd9a85fcf726a2ae7f2136
YuyaYamazi <kaimomo12345@gmail.com>
2023-11-24 01:33:13 +0900

    createpost

--- src/components/Createpost.jsx
+++ src/components/Createpost.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Createpost = () => {
+  const [title, setTitle] = useState()
+  const [postText, setPostText] = useState()
+
+  const createPost = () => {
+    console.log(title,postText)
+  }
+
   return (
     <>
       <section className="text-gray-600 w-full flex flex-col items-center px-2">
       <h1 className="text-3xl font-bold mt-10">記事を投稿する</h1>
-      <form className="shadow-md rounded-md bg-white w-full max-w-2xl p-10">
+      {/* <form className="shadow-md rounded-md bg-white w-full max-w-2xl p-10"> */}
         <div className="flex sm:items-center mb-6 flex-col sm:flex-row">
           <label
             className="block sm:w-1/3 font-bold sm:text-right mb-1 pr-4"
@@ -16,6 +23,7 @@ const Createpost = () => {
             id="name"
             type="text"
             placeholder="タイトルの記入"
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
   
@@ -32,13 +40,14 @@ const Createpost = () => {
             cols="30"
             rows="10"
             placeholder='投稿内容を記入'
+            onChange={(e) => setPostText(e.target.value)}
           >
           </textarea>
         </div>
         <div className='flex justify-end'>
-        <button  className='border border-sky-500 bg-sky-500 text-white px-1 py-2 rounded-full'>投稿する</button>
+        <button onClick={createPost}  className='border border-sky-500 bg-sky-500 text-white px-1 py-2 rounded-full'>投稿する</button>
         </div>
-      </form>
+      {/* </form> */}
     </section>
     </>
   )
