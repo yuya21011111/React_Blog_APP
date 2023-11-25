24d1c6e51046f2b16c415de16ac129f53dfcc3e6
YuyaYamazi <kaimomo12345@gmail.com>
2023-11-25 15:27:37 +0900

    Homeデザイン

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,38 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+  <div className='flex justify-center'>
+ <div className="mt-16 w-96  flex flex-col  bg-white border shadow-sm rounded-xl dark:bg-slate-700 dark:border-gray-900 dark:shadow-slate-700/[.7]">
+  <div className="p-4 md:p-5">
+    <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+      タイトル
+    </h3>
+    <p className="mt-1 text-gray-500 dark:text-gray-400">
+      今はReactの学習中になります
+      今はReactの学習中になります
+      今はReactの学習中になります
+      今はReactの学習中になります
+      今はReactの学習中になります
+      今はReactの学習中になります
+      今はReactの学習中になります
+      今はReactの学習中になります
+      今はReactの学習中になります
+      今はReactの学習中になります
+      今はReactの学習中になります
+      今はReactの学習中になります
+      今はReactの学習中になります
+      今はReactの学習中になります
+      今はReactの学習中になります
+    </p>
+    <p className="mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+      @Yublog
+    </p>
+  </div>
+  <div className='text-center'>
+    <button className='border border-red-400 bg-red-400 text-white px-1 py-2 rounded-full'>削除</button>
+  </div>
+</div>
+</div>
   )
 }
 
